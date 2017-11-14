92f9f61bf649d330ce676d405aebb834670cc7ee
Namasang <namayz18@gmail.com>
2017-11-14 17:10:16 +0545

    fetchWater

---
+++ OnOFF/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  OnOFF
+//
+//  Created by Namasang Yonzan on 14/11/17.
+//  Copyright © 2017 Namasang Yonzan. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+class ViewController: UIViewController {
+    //MARK: Variables
+    var ref: DatabaseReference!
+    var value: Int!
+    //MARK: Outlets
+    @IBOutlet weak var device: UILabel!
+    @IBOutlet weak var `switch`: UISwitch!
+    //MARK: VC life
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Database.database().reference()
+        self.switch.isHidden = true
+        deviceStatus()
+    }
+    //MARK: Device Status
+    func deviceStatus()  {
+        ref.child("device").observe(.value) { (snapshot) in
+            let value = snapshot.value as! [String:Any]
+            let status = value["value"] as? Int ?? 0
+            self.switch.isHidden = false
+            status == 1 ? self.on() : self.off()
+        }
+    }
+    //MARK: Device is ON
+    func on() {
+        self.device.text = "ON"
+        self.switch.isOn = true
+    }
+    //MARK: Device is off
+    func off() {
+        self.device.text = "OFF"
+        self.switch.isOn = false
+    }
+    //MARK: Switch action
+    @IBAction func `switch`(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            value = 1
+            setDevice()
+        }else {
+            value = 0
+            setDevice()
+        }
+    }
+    //MARK: SetValue
+    func setDevice() {
+        self.ref.child("device").setValue(["value": value])
+    }
+    
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-OnOFF/Pods-OnOFF-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OnOFF : NSObject
+@end
+@implementation PodsDummy_Pods_OnOFF
+@end
